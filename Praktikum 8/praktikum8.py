85f4cc0410148c55c1414b8169481ffe916a5a26
yogafrtm25 <yogafrtm25@gmail.com>
2022-12-13 14:08:08 +0700

    Menambahkan Penjelasan

---
+++ Praktikum 8/praktikum8.py
@@ -0,0 +1,97 @@
+class mahasiswa:
+    def __init__(self, nim, nama, tugas, uts, uas):
+        self.nim = nim
+        self.nama = nama
+        self.tugas = tugas
+        self.uts = uts
+        self.uas = uas
+
+    def tambah(self,nim,nama,tugas,uts,uas):
+        data.nim.append(nim)
+        data.nama.append(nama)
+        data.tugas.append(tugas)
+        data.uts.append(uts)
+        data.uas.append(uas)
+
+    def lihat(self):
+        for i in range(len(data.nama)):
+            print("|", i+1, "  |", end="")
+            print('{0:<25}'.format(self.nama[i]), end="")
+            print("|", self.nim[i], end="")
+            print(" |", self.tugas[i], end="")
+            print("    |", self.uts[i], end="")
+            print("  |", self.uas[i], " | ", end="")
+            print(f'{((self.tugas[i]*30/100) + (self.uts[i]*35/100) + (self.uas[i]*35/100)) :.2f}', " |")
+
+    def ubah(self,nim,nama,tugas,uts,uas):
+        self.nim[no] = nim
+        self.nama[no] = nama
+        self.tugas[no] = tugas
+        self.uts[no] = uts
+        self.uas[no] = uas
+
+    def hapus(self):
+        del self.nim[no]
+        del self.nama[no]
+        del self.tugas[no]
+        del self.uts[no]
+        del self.uas[no]
+
+data = mahasiswa([],[],[],[],[])
+
+while True:
+    menu = input("\n[(L)ihat, (T)ambah, (U)bah, (H)apus, (K)eluar]:")
+    if menu == "t" or menu == "T":
+       print("\nTambah Data")
+       data.tambah(
+           input("Masukkan NIM : "),
+           input("Masukkan Nama : "),
+           int(input("Nilai Tugas : ")),
+           int(input("Nilai UTS : ")),
+           int(input("Nilai UAS : "))
+           )
+       print("\nData berhasil ditambahkan")
+
+    elif menu == "l" or menu == "L":
+        print("\nDaftar Nilai")
+        print("==========================================================================")
+        print("| No  |          Nama           |    NIM    | TUGAS | UTS | UAS |  AKHIR |")
+        print("==========================================================================")
+        if len(data.nama) != 0:
+            data.lihat()
+        else:
+            print("                         TIDAK ADA DATA                               ")
+        print("==========================================================================")
+
+    elif menu == "u" or menu == "U":
+        print("\nUbah Data")
+        ubah = input("Masukkan Nama : ")
+        if ubah in data.nama:
+           no = data.nama.index(ubah)
+           print("Masukkan Data Yang Baru : ")
+           data.ubah(
+               input("NIM : "),
+               input("Nama : "),
+               int(input("Nilai Tugas : ")),
+               int(input("Nilai UTS : ")),
+               int(input("Nilai UAS : "))
+               )
+        else:
+            print(ubah, "tidak ada di dalam data")
+
+    elif menu == "h" or menu == "H":
+        print("\nHapus Data")
+        hapus = input("Masukkan Nama : ")
+        if hapus in data.nama:
+            no = data.nama.index(hapus)
+            data.hapus()
+            print("Data", hapus, "Berhasil dihapus")
+        else:
+            print(hapus, "tidak ada di dalam data")
+
+    elif menu == "k" or menu == "K":
+        print("\nTerimakasih\n")
+        break
+
+    else:
+        print("\nPerintah yang dimasukkan salah!\n")
